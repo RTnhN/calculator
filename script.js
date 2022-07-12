1b921658734e9c7fd4f89b4c23e3f5ab3161134d
Zach Strout <zachstrout@gmail.com>
2022-07-11 22:22:45 -0500

    Format code and fix names
    This just fixes spaces, semicolons, and brackets while also changing the
    names on some functions.
    
    THERE ARE NO FUNCTIONAL CHANGES IN THIS COMMIT.

--- script.js
+++ script.js
@@ -9,35 +9,35 @@ const plusMinusButton = document.querySelector(".plusMinus");
 const miscFuncs = document.querySelectorAll(".miscFunc");
 
 let validNumberEntered = null;
-let allowableChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0","Enter", "/", "*", "-", "+", "=", "^", ".", "Backspace", "Delete","%"];
+let allowableChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Enter", "/", "*", "-", "+", "=", "^", ".", "Backspace", "Delete", "%"];
 let operatorChars = "+-*/^";
 
 const calculator = {
-  operandOne:null, 
-  operandTwo:null,
-  operator:null, 
-  result:null,
+  operandOne: null,
+  operandTwo: null,
+  operator: null,
+  result: null,
   add() {
-    this.result = this.operandOne + this.operandTwo; 
+    this.result = this.operandOne + this.operandTwo;
     return this.result;
-  }, 
+  },
   subtract() {
-    this.result = this.operandOne - this.operandTwo; 
+    this.result = this.operandOne - this.operandTwo;
     return this.result;
   },
   multiply() {
     this.result = this.operandOne * this.operandTwo;
     return this.result;
-  }, 
+  },
   divide() {
     this.result = this.operandOne / this.operandTwo;
     return this.result;
   },
-  power(){
+  power() {
     this.result = this.operandOne ** this.operandTwo;
     return this.result;
   },
-  equals(){
+  equals() {
     switch (this.operator) {
       case "+":
         return this.add();
@@ -52,226 +52,227 @@ const calculator = {
         return this.divide();
         break;
       case "^":
-          return this.power();
-          break;  
-      }
+        return this.power();
+        break;
+    }
   },
-  resetCalculator(){
+  resetCalculator() {
     this.operandOne = null;
     this.operandTwo = null;
     this.operator = null;
     this.result = null;
   },
-  readyForCalculations(){
-    return (this.operandOne !== null && this.operandTwo !== null && this.operator !== null)
+  readyToCalculate() {
+    return (this.operandOne !== null && this.operandTwo !== null && this.operator !== null);
   }
-} 
+}
 
 clearButton.addEventListener("click", clearDisplay);
-numbers.forEach(number=>number.addEventListener("click", numberPressed));
-operationButtons.forEach(operation=>operation.addEventListener("click", addOperation));
-equalsButton.addEventListener("click", showResult);
+numbers.forEach(number => number.addEventListener("click", numberPressed));
+operationButtons.forEach(operation => operation.addEventListener("click", operationPressed));
+equalsButton.addEventListener("click", equalsPressed);
 document.addEventListener("keydown", keyPressed);
 backspaceButton.addEventListener("click", removeRightChar);
 percentButton.addEventListener("click", calculatePercent);
 plusMinusButton.addEventListener("click", changeSign);
 
 function numberPressed(e) {
-  if (validNumberEntered !== true){
+  if (validNumberEntered !== true) {
     display.textContent = "";
-    if (e.target.textContent !=="0"){
+    if (e.target.textContent !== "0") {
       validNumberEntered = true;
     }
-  };  
-  if (display.clientWidth > 300){
+  };
+  if (display.clientWidth > 300) {
     return;
   }
-  if (display.textContent.includes(".") && e.target.textContent==="."){
+  if (display.textContent.includes(".") && e.target.textContent === ".") {
     return
   }
-  if (display.textContent === "" && e.target.textContent==="."){
+  if (display.textContent === "" && e.target.textContent === ".") {
     display.textContent = "0";
   };
-  if (calculator.result !== null){
+  if (calculator.result !== null) {
     calculator.resetCalculator();
   }
   display.textContent += e.target.textContent;
-  if (calculator.operator === null){
+  if (calculator.operator === null) {
     calculator.operandOne = +display.textContent;
-  } else{
+  } else {
     calculator.operandTwo = +display.textContent;
   }
 }
 
-function clearDisplay(){
+function clearDisplay() {
   display.textContent = "0";
   validNumberEntered = false;
   calculator.resetCalculator();
-  operationButtons.forEach(operator=> operator.classList.remove("pressedOperation"))
+  operationButtons.forEach(operator => operator.classList.remove("pressedOperation"))
 }
 
-function addOperation(e){
-  if (calculator.operandTwo !== null){
+function operationPressed(e) {
+  if (calculator.operandTwo !== null) {
     calculator.operandOne = calculator.equals();
     calculator.operandTwo = null;
     display.textContent = calculator.result;
     calculator.result = null;
-  } 
-  if (e.type === "click"){
+  }
+  if (e.type === "click") {
     calculator.operator = e.target.textContent;
-  } else{
-    if (e.key === "*"){
-      calculator.operator ="×";
-    } else if (e.key === "/"){
-      calculator.operator ="÷";
+  } else {
+    if (e.key === "*") {
+      calculator.operator = "×";
+    } else if (e.key === "/") {
+      calculator.operator = "÷";
     } else {
       calculator.operator = e.key;
     }
   }
-  updateOperatorColor(calculator.operator)
+  updateOperatorColor(calculator.operator);
   validNumberEntered = false;
 }
 
-function showResult() {
-  if (!calculator.readyForCalculations()){
+function equalsPressed() {
+  if (!calculator.readyToCalculate()) {
     display.textContent = "ERROR";
     calculator.resetCalculator();
     validNumberEntered = false;
-  } else{
+  } else {
     display.textContent = calculator.equals();
-    if (display.clientWidth > 300){
-      display.textContent = calculator.equals().toExponential(6)
+    if (display.clientWidth > 300) {
+      display.textContent = calculator.equals().toExponential(6);
     }
     validNumberEntered = false;
-    
+
   }
-  operationButtons.forEach(operator=> operator.classList.remove("pressedOperation"))
+  operationButtons.forEach(operator => operator.classList.remove("pressedOperation"));
 }
 
 function removeRightChar() {
-  display.textContent = display.textContent.slice(0,-1);
-  if (display.textContent === ""){
+  display.textContent = display.textContent.slice(0, -1);
+  if (display.textContent === "") {
     display.textContent = "0";
     validNumberEntered = false;
   }
+
 }
 
 function keyPressed(e) {
-  if (!allowableChars.includes(e.key)){
+  if (!allowableChars.includes(e.key)) {
     return;
   }
-  if (operatorChars.includes(e.key)){
-    addOperation(e);
+  if (operatorChars.includes(e.key)) {
+    operationPressed(e);
     return;
   }
-  if (e.key === "Enter"){
-    showResult()
+  if (e.key === "Enter") {
+    equalsPressed()
     return;
   }
-  if (e.key === "Backspace"){
+  if (e.key === "Backspace") {
     removeRightChar();
     return;
   }
-  if (e.key === "Delete"){
+  if (e.key === "Delete") {
     clearDisplay();
     return;
   }
-  if (e.key === "%"){
+  if (e.key === "%") {
     calculatePercent();
     return;
   }
-  if (validNumberEntered !== true){
+  if (validNumberEntered !== true) {
     display.textContent = "";
     validNumberEntered = true;
-  };  
-  if (display.textContent.includes(".") && e.key==="."){
+  };
+  if (display.textContent.includes(".") && e.key === ".") {
     return
   }
-  if (display.textContent === "" && e.key==="."){
+  if (display.textContent === "" && e.key === ".") {
     display.textContent = "0";
-  };  
-  if (calculator.result !== null){
+  };
+  if (calculator.result !== null) {
     calculator.resetCalculator();
   }
   display.textContent += e.key;
-  if (calculator.operator === null){
+  if (calculator.operator === null) {
     calculator.operandOne = +display.textContent;
-  } else{
+  } else {
     calculator.operandTwo = +display.textContent;
   }
 }
 
 function calculatePercent() {
-  display.textContent = +display.textContent/100;
-  if (calculator.operandTwo !== null){
-    calculator.operandTwo = calculator.operandTwo/100;
+  display.textContent = +display.textContent / 100;
+  if (calculator.operandTwo !== null) {
+    calculator.operandTwo = calculator.operandTwo / 100;
   } else {
-    calculator.operandOne = calculator.operandOne/100;
+    calculator.operandOne = calculator.operandOne / 100;
   }
 
 }
 
-function changeSign(){
-  if (display.textContent === "0"){
+function changeSign() {
+  if (display.textContent === "0") {
     return;
   }
-  if (display.textContent[0] !== "-"){
-    display.textContent = "-" + display.textContent
-  } else{
+  if (display.textContent[0] !== "-") {
+    display.textContent = "-" + display.textContent;
+  } else {
     display.textContent = display.textContent.substring(1);
   }
-  if (calculator.operator === null){
+  if (calculator.operator === null) {
     calculator.operandOne = +display.textContent;
-  } else{
+  } else {
     calculator.operandTwo = +display.textContent;
   }
 }
 
-function updateOperatorColor(operator){
-  operationButtons.forEach(operator=> operator.classList.remove("pressedOperation"))
+function updateOperatorColor(operator) {
+  operationButtons.forEach(operator => operator.classList.remove("pressedOperation"));
   switch (operator) {
     case "+":
-      operationButtons[3].classList.add("pressedOperation")
+      operationButtons[3].classList.add("pressedOperation");
       break;
     case "-":
-      operationButtons[2].classList.add("pressedOperation")
+      operationButtons[2].classList.add("pressedOperation");
       break;
     case "×":
-      operationButtons[1].classList.add("pressedOperation")
+      operationButtons[1].classList.add("pressedOperation");
       break;
     case "÷":
-      operationButtons[0].classList.add("pressedOperation")
+      operationButtons[0].classList.add("pressedOperation");
       break;
-    }
+  }
 }
 
 
-numbers.forEach(number=>number.addEventListener("mousedown", numPressedColor));
-numbers.forEach(number=>number.addEventListener("mouseup", numReleasedColor));
+numbers.forEach(number => number.addEventListener("mousedown", numPressedColor));
+numbers.forEach(number => number.addEventListener("mouseup", numReleasedColor));
 
-function numPressedColor(e){
+function numPressedColor(e) {
   e.target.classList.remove("releasedNumber");
   e.target.classList.add("pressedNumber");
 }
-function numReleasedColor(e){
+function numReleasedColor(e) {
   e.target.classList.add("releasedNumber");
   e.target.classList.remove("pressedNumber");
 }
 
-miscFuncs.forEach(func=>func.addEventListener("mousedown", funcPressedColor));
-miscFuncs.forEach(func=>func.addEventListener("mouseup", funcReleasedColor));
+miscFuncs.forEach(func => func.addEventListener("mousedown", funcPressedColor));
+miscFuncs.forEach(func => func.addEventListener("mouseup", funcReleasedColor));
 
-function funcPressedColor(e){
+function funcPressedColor(e) {
   e.target.classList.remove("releasedFunc");
   e.target.classList.add("pressedFunc");
 }
-function funcReleasedColor(e){
+function funcReleasedColor(e) {
   e.target.classList.add("releasedFunc");
   e.target.classList.remove("pressedFunc");
 }
 
-if(window.innerWidth > 500){
+if (window.innerWidth > 500) {
   document.body.style.alignItems = "center";
-} else{
+} else {
   document.body.style.alignItems = "flex-end";
 }
